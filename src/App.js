c9ff637a06ad59fad68abbda52ae1ea3ff2c99ab
Kaami Dev <kaami@kaamidev.com>
2020-06-22 19:16:08 -0400

    created and styled platform card on homepage

--- src/App.js
+++ src/App.js
@@ -14,12 +14,14 @@ function App() {
 		<div>
 			<Router>
 				<Header />
-				<Switch>
-					<Route path="/" exact component={Home} />
-					<Route path="/add" component={Add} />
-					<Route path="/edit" component={Edit} />
-					<Route path="/use" component={Use} />
-				</Switch>
+				<div className="body-section">
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/add" component={Add} />
+						<Route path="/edit" component={Edit} />
+						<Route path="/use" component={Use} />
+					</Switch>
+				</div>
 			</Router>
 		</div>
 	);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import discordImg from '../images/discord.png';
+import slackImg from '../images/slack.png';
 
 const Home = () => {
 	return (
-		<div>
-			<p>Home Page</p>
+		<div className="home-container">
+			<div className="card platform-card">
+				<h5 className="platform-title">Select a platform below.</h5>
+				<div className="platform-link-container">
+					<Link className="platform-link" to="/add">
+						<img alt="discord-img" src={discordImg} />
+						<p>Discord</p>
+					</Link>
+					<Link className="platform-link" to="/add">
+						<img alt="discord-img" src={slackImg} />
+						<p>Slack</p>
+					</Link>
+				</div>
+			</div>
+			{/* <div className="home-flex-container">
+				<div className="home-about-row">
+					<div className="about-card card">
+						<p>.</p>
+					</div>
+				</div>
+				<div className="home-saved-row">
+					<div className="saved-card card">
+						<p>.</p>
+					</div>
+					<div className="saved-card card">
+						<p>.</p>
+					</div>
+				</div>
+			</div> */}
 		</div>
 	);
 };
