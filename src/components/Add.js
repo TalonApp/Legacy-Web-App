03ee158e0eceedb1f029f04c32604c0a92c6dc3a
Kaami Dev <kaami@kaamidev.com>
2020-06-23 02:51:49 -0400

    created & styled add webhook page

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import discordImg from '../images/discord-lg.png';
 
 const Add = () => {
 	return (
-		<div>
-			<p>Add Page</p>
+		<div className="add-container">
+			<div className="card add-card">
+				<div className="add-card-heading">
+					<img alt="service-img" src={discordImg} />
+					<div className="add-card-heading-text">
+						<h3>Setup Webhook</h3>
+						<p>Add a discord webhook below.</p>
+					</div>
+				</div>
+				<div className="add-card-body">
+					<input type="text" placeholder="Webhook Name" />
+					<input type="url" placeholder="Webhook URL" />
+					<input type="text" placeholder="Override Username (optional)" />
+					<input type="text" placeholder="Override Profile Pic (optional)" />
+					<div className="add-card-btn-container">
+						<button className="btn about-card-btn">Save & Use</button>
+						<button className="btn about-card-btn">Back</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
