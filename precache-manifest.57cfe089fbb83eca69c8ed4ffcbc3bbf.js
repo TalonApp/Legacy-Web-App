5dff0f469c7fe2461ec1bc08c4fd8357b9e5ff2f
Kaami Dev <kaami@kaamidev.com>
2020-06-24 23:52:00 -0400

    Updates

---
+++ precache-manifest.57cfe089fbb83eca69c8ed4ffcbc3bbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0984269597abbe4b822198fe3721b88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8357ed9531ed34dc33e8",
+    "url": "/static/css/main.8422c58d.chunk.css"
+  },
+  {
+    "revision": "2a83406add3da034d82e",
+    "url": "/static/js/2.9258d9ff.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/static/js/2.9258d9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8357ed9531ed34dc33e8",
+    "url": "/static/js/main.bab2eff6.chunk.js"
+  },
+  {
+    "revision": "967cc1d82f7736c338f7",
+    "url": "/static/js/runtime-main.248e8f1a.js"
+  }
+]);
